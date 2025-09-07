438b09c2a59b6aacfae1e3ea8d6fffc9a953686a
Hamza amine amil <hamza.amineamil@gmail.com>
2025-09-07 21:30:50 +0100

    third task commit

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from django.contrib.auth import views as auth_views
-from . import views  
+from django.views.generic import RedirectView
+from .views import list_books, LibraryDetailView, register
+from django.contrib.auth.views import LoginView, LogoutView  
 
 urlpatterns = [
    
@@ -8,9 +9,7 @@ urlpatterns = [
     path("libraries/<int:pk>/", views.LibraryDetailView.as_view(), name="library_detail"),
 
     
-    path("login/",  auth_views.LoginView.as_view(template_name="relationship_app/login.html"), name="login"),
-    path("logout/", auth_views.LogoutView.as_view(next_page="login"), name="logout"),
-
- 
-    path("register/", views.register, name="register"),
+    path("login/", LoginView.as_view(template_name="relationship_app/login.html"), name="login"),
+    path("logout/", LogoutView.as_view(template_name="relationship_app/logout.html"), name="logout"),
+    path("register/", register.as_view()
 ]
