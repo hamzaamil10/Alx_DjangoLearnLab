0c651345dd3d8fb15e75aa96187f334b995520cb
Hamza amine amil <hamza.amineamil@gmail.com>
2025-09-07 16:35:28 +0100

    second task commit

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from . import views
 from .views import LibraryDetailView
+from .views import list_books
 
 urlpatterns = [
     path('books/', views.list_books, name='list_books'),
