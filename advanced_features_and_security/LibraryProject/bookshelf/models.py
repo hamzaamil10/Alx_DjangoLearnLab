9949a98c40f390db0f48d5fdb49792956b514c43
Hamza amine amil <hamza.amineamil@gmail.com>
2025-09-13 11:17:16 +0100

    first commit

--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -9,7 +9,25 @@ class Book(models.Model):
     def __str__(self):
         return f"{self.title} by {self.author} ({self.publication_year})"
     
+class CustomUserManager(BaseUserManager):
+    use_in_migrations = True
+    def create_user(self, username, email=None, password=None, **extra_fields):
+        if not username:
+            raise ValueError("The username must be set")
+        email = self.normalize_email(email)
+        user = self.model(username=username, email=email, **extra_fields)
+        user.set_password(password) if password else user.set_unusable_password()
+        user.save(using=self._db)
+        return user
 
+    def create_superuser(self, username, email=None, password=None, **extra_fields):
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+        extra_fields.setdefault("is_active", True)
+        if extra_fields["is_staff"] is not True or extra_fields["is_superuser"] is not True:
+            raise ValueError("Superuser must have is_staff=True and is_superuser=True.")
+        return self.create_user(username, email, password, **extra_fields)
+    
 class CustomUser(AbstractUser):
     date_of_birth = models.DateField(null=True, blank=True)
     profile_photo = models.ImageField(upload_to="profiles/", null=True, blank=True)
