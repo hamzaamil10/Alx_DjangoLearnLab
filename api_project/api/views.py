b671efb5496f7a9bf40de739375abe33987eaf86
Hamza amine amil <hamza.amineamil@gmail.com>
2025-09-17 15:37:51 +0100

    feat : changing file name

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,6 +1,6 @@
 from rest_framework import generics
 from .models import Book
-from .serializer import BookSerializer
+from .serializers import BookSerializer
 
 class BookList(generics.ListAPIView):
     queryset = Book.objects.all()
