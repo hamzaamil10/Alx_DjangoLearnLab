eba627207a0d8b45ed12dee190cc470290d78d79
Hamza amine amil <hamza.amineamil@gmail.com>
2025-09-26 12:16:54 +0100

    feat: delivering Third task

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django_filters import rest_framework
 from rest_framework import generics, permissions, filters
 from django_filters.rest_framework import DjangoFilterBackend
 from .models import Author, Book
